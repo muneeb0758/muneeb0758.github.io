20421ab4fada02a5da0928c46904bb8a43061658
muneeb0758 <136179830+muneeb0758@users.noreply.github.com>
2023-09-27 11:45:45 +0100

    All New Design
    Footer and background added with addition to responsive enhancements

--- script.js
+++ script.js
@@ -1,58 +1,79 @@
-
-
+// Get the button and text elements
 const btn = document.querySelector(".abt");
 const text = document.querySelector(".abt-text");
 
+// Add the "show-text" class to the text element
+text.classList.add("show-text");
+
+// Add a click event listener to the button
 btn.addEventListener("click", () => {
-  // Remove 'show-text' class from all text elements
+  // Check if the other two buttons have the `show-text` class
+  if (text2.classList.contains("show-text")) {
+    text2.classList.remove("show-text");
+  }
+  if (text3.classList.contains("show-text")) {
+    text3.classList.remove("show-text");
+  }
+
+  // Remove the "show-text" class from all text elements
   document.querySelectorAll(".text-element").forEach((element) => {
     element.classList.remove("show-text");
   });
 
-  // Add 'show-text' class to the clicked text element
+  // Add the "show-text" class to the clicked text element
   text.classList.toggle("show-text");
 });
 
+// Hide all sections except for the services section
+document.querySelectorAll("section:not(.services)").forEach((section) => {
+  section.style.opacity = 0;
+  section.style.transition = "opacity 0.5s";
+});
+
+// Add click event listeners to the other two buttons
 const btn2 = document.querySelector(".skl");
 const text2 = document.querySelector(".skl-text");
 
 btn2.addEventListener("click", () => {
-  // Remove 'show-text' class from all text elements
+  // Remove the "show-text" class from all text elements
   document.querySelectorAll(".text-element").forEach((element) => {
     element.classList.remove("show-text");
   });
 
-  // Add 'show-text' class to the clicked text element
+  // Add the "show-text" class to the clicked text element
   text2.classList.toggle("show-text");
+
+  // Remove the "show-text" class from the other two text elements
+  if (text.classList.contains("show-text")) {
+    text.classList.remove("show-text");
+  }
+  if (text3.classList.contains("show-text")) {
+    text3.classList.remove("show-text");
+  }
 });
 
 const btn3 = document.querySelector(".exp");
 const text3 = document.querySelector(".exp-text");
 
 btn3.addEventListener("click", () => {
-  // Remove 'show-text' class from all text elements
+  // Remove the "show-text" class from all text elements
   document.querySelectorAll(".text-element").forEach((element) => {
     element.classList.remove("show-text");
   });
 
-  // Add 'show-text' class to the clicked text element
+  // Add the "show-text" class to the clicked text element
   text3.classList.toggle("show-text");
+
+  // Remove the "show-text" class from the other two text elements
+  if (text.classList.contains("show-text")) {
+    text.classList.remove("show-text");
+  }
+  if (text2.classList.contains("show-text")) {
+    text2.classList.remove("show-text");
+  }
 });
 
 document.querySelectorAll("section:not(.services)").forEach((section) => {
   section.style.opacity = 0;
   section.style.transition = "opacity 0.5s";
 });
-
-{
-const topButton = document.getElementById("back-to-top");
-
-topButton.addEventListener("click", () => {
-  window.scrollTo({
-    top: 0,
-    behavior: "smooth",
-  });
-});
-}
-
-
